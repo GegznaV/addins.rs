f4c85c2c3b15115311b648210d563699663cc099
Vilmantas Gegzna <GegznaV@gmail.com>
2018-09-08 16:49:24 +0300

    Functions added and renamed

---
+++ R/enclose.R
@@ -0,0 +1,13 @@
+#' Enclose with \%\%.
+#'
+#' Call this function as an add-in to enclose selected text with percent operator \code{ \% }.
+#'
+#' @inheritParams addin.tools::rs_get_ind
+#'
+#' @export
+#'
+#' @family 'Insert at cursor position' add-ins
+#'
+rs_enclose_with_percent <- function(context = rs_get_context()) {
+    rs_enclose_selection_with(symbol = "%")
+}
--- R/insert_operators_other.R
+++ R/insert_operators_other.R
@@ -1,9 +1,8 @@
-
-# Base R operators ------------------------------------------------------------
-
-
-
 # =============================================================================
+# Base R operators ------------------------------------------------------------
+# =============================================================================
+
+
 #' Insert \%*\%.
 #'
 #' Call this function as an add-in to insert \code{ \%*\% } at the cursor position.
@@ -17,6 +16,35 @@
 rs_insert_matrix_multiplication <- function(context = rs_get_context()) {
     # rstudioapi::insertText(text = " %*% ")
     rs_insert_text(text = "%*%", context = context, spaces = TRUE)
+}
+
+#' Insert \%/\%.
+#'
+#' @description Call this function as an add-in to insert \code{ \%/\% } at the cursor position.
+#'
+#' @inheritParams addin.tools::rs_get_ind
+#' @export
+#' @seealso Operator \link[base]{\%/\%}.
+#'
+#' @family 'Insert at cursor position' add-ins
+rs_insert_integer_division <- function(context = rs_get_context()) {
+    rs_insert_text(text = "%/%", context = context, spaces = TRUE)
+}
+
+#' Insert \%\%.
+#'
+#' Call this function as an add-in to insert \code{ \%\% } at the cursor position.
+#'
+#' @note Operator does not work unless package which contains this operator is loaded.
+#'
+#' @inheritParams addin.tools::rs_get_ind
+#'
+#' @export
+#'
+#' @family 'Insert at cursor position' add-ins
+#'
+rs_insert_modulo <- function(context = rs_get_context()) {
+    rs_insert_text(text = "%%", context = context, spaces = TRUE)
 }
 
 
@@ -34,6 +62,10 @@ rs_insert_infix_in <- function(context = rs_get_context()) {
     rs_insert_text(text = "%in%", context = context, spaces = TRUE)
 }
 
+# =============================================================================
+# Operators from other packages -----------------------------------------------
+# =============================================================================
+
 #' Insert \%!in\%.
 #'
 #' Call this function as an add-in to insert \code{ \%!in\% } at the cursor position.
@@ -50,15 +82,14 @@ rs_insert_infix_not_in <- function(context = rs_get_context()) {
     rs_insert_text(text = "%!in%", context = context, spaces = TRUE)
 }
 
-
-
+#
 #' Insert \%R\%.
 #'
 #' Call this function as an add-in to insert \code{ \%R\% } at the cursor position.
 #'
 #' @inheritParams addin.tools::rs_get_ind
 #'
-#' @note Operator does not work unless library which contains this operator is loaded.
+#' @note Operator does not work unless package which contains this operator is loaded.
 #'
 #' @export
 #'
@@ -68,20 +99,4 @@ rs_insert_infix_not_in <- function(context = rs_get_context()) {
 rs_insert_infix_R <- function(context = rs_get_context()) {
     # rstudioapi::insertText(text = " %R% ")
     rs_insert_text(text = "%R%", context = context, spaces = TRUE)
-}
-
-#' Insert \%\%.
-#'
-#' Call this function as an add-in to insert \code{ \%\% } at the cursor position.
-#'
-#' @note Operator does not work unless library which contains this operator is loaded.
-#'
-#' @inheritParams addin.tools::rs_get_ind
-#'
-#' @export
-#'
-#' @family 'Insert at cursor position' add-ins
-#'
-rs_insert_infix_operator <- function(context = rs_get_context()) {
-    rs_enclose_selection_with(symbol = "%")
 }
