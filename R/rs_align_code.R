9fe20cae44db98dfbcbe14e57b4274c2a4f3c00f
Vilmantas Gegzna <GegznaV@gmail.com>
2019-07-31 17:04:51 +0300

    Update rs_align_code.R

--- R/rs_align_code.R
+++ R/rs_align_code.R
@@ -1,19 +1,65 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# @rdname align_code
+# @export
+
+# TODO: create new algorithm that aligns in selections only.
+
+# rs_align_code__in_selectios <- function(at_symbol, context = rs_get_context()) {
+#
+#
+#
+# text        <- rs_get_selected_rows(context = context)
+# row_numbers <- attr(text, "row_numbers")
+#
+# rez <-
+#     stringr::str_locate(
+#         pattern = at_symbol,
+#         string  = text) %>%
+#     as.data.frame() %>%
+#     dplyr::mutate(
+#         row    = row_numbers,
+#         shift  = max(start, na.rm = TRUE) - start, # shift = n spaces to insert
+#         spaces = make_spaces(shift) # spaces that create alignment
+#     ) %>%
+#     dplyr::filter(shift > 0) %>% # only rows which should be modiffied
+#     dplyr::rename(col = start) %>%
+#     dplyr::select(row, col, spaces)
+#
+# # Put indices into correct format
+# locations <- split(as.matrix(rez[, c("row", "col")]), rez$row)
+#
+# purrr::walk2(
+#     locations, rez$spaces,
+#     ~ rstudioapi::insertText(.x, .y, context$id))
+# }
+
+
+
+# ============================================================================
 #' Align code at certain symbols.
 #'
-#' Align code of selected lines at first occurance of certain symbol.
+#' In selected lines, align code at first occurance of certain symbol in each row.
 #'
 #' @note
-#' Inspired by: \url{https://github.com/seasmith/AlignAssign}
+#' Inspired by \url{https://github.com/seasmith/AlignAssign}.
 #'
-#' @name rs_align_code
-#' @param at_symbol (character) A regular expression that represents
-#'  symbol treated as reference for code alignment.
-#' @param context Object with context of active RStudio document
-#'                (class \code{document_context}).
+#' @name align_code
 #'
+#' @param at_symbol (character) \cr
+#'        A regular expression that represents symbol treated as reference for
+#'        code alignment.
+#'
+#' @param context (class \code{document_context}) \cr
+#'        Object with context of active RStudio document.
+
+NULL
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname align_code
 #' @export
-#'
+#
+# This version of function will deprecate in the near future in favor to changes
+# in selections only.
 rs_align_code <- function(at_symbol, context = rs_get_context()) {
     text        <- rs_get_selected_rows(context = context)
     row_numbers <- attr(text, "row_numbers")
@@ -22,9 +68,10 @@ rs_align_code <- function(at_symbol, context = rs_get_context()) {
         stringr::str_locate(pattern = at_symbol,
                             string  = text) %>%
         as.data.frame() %>%
-        dplyr::mutate(row    = row_numbers,
-                      shift  = max(start, na.rm = TRUE) - start, # shift = n spaces to insert
-                      spaces = make_spaces(shift) # spaces that create alignment
+        dplyr::mutate(
+            row    = row_numbers,
+            shift  = max(start, na.rm = TRUE) - start, # shift = n spaces to insert
+            spaces = make_spaces(shift) # spaces that create alignment
         ) %>%
         dplyr::filter(shift > 0) %>% # only rows which should be modiffied
         dplyr::rename(col = start) %>%
@@ -33,31 +80,38 @@ rs_align_code <- function(at_symbol, context = rs_get_context()) {
     # Put indices into correct format
     locations <- split(as.matrix(rez[, c("row", "col")]), rez$row)
 
-    purrr::walk2(locations, rez$spaces,
-                 ~ rstudioapi::insertText(.x, .y, context$id))
+    purrr::walk2(
+        locations, rez$spaces,
+        ~ rstudioapi::insertText(.x, .y, context$id))
 }
-
-#' @rdname rs_align_code
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname align_code
 #' @export
 rs_align_code_at_equal <- function(context = rs_get_context()) {
     rs_align_code(stringr::fixed("="), context = context)
 }
-
-#' @rdname rs_align_code
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname align_code
 #' @export
 rs_align_code_at_arrow <- function(context = rs_get_context()) {
     rs_align_code(stringr::fixed("<-"), context = context)
 }
-
-#' @rdname rs_align_code
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname align_code
 #' @export
-rs_align_code_at_pipe <- function(context = rs_get_context()) {
+rs_align_code_at_pipe_operator <- function(context = rs_get_context()) {
     rs_align_code(stringr::fixed("%>%"), context = context)
 }
-
-#' @rdname rs_align_code
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname align_code
 #' @export
 rs_align_code_at_hash <- function(context = rs_get_context()) {
     rs_align_code(stringr::fixed("#"), context = context)
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname align_code
+#' @export
+rs_align_code_at_vertical_bar <- function(context = rs_get_context()) {
+    rs_align_code(stringr::fixed("|"), context = context)
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
