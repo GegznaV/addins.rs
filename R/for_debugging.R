3c51e5a4d2b6f93a5a61a17dc9645ee309404bed
Vilmantas Gegzna <GegznaV@gmail.com>
2020-06-08 21:50:53 +0300

    Update for_debugging.R

--- R/for_debugging.R
+++ R/for_debugging.R
@@ -3,6 +3,11 @@ addins_env <- new.env()
 rs_get_context_addin <- function(variables) {
   rstudio_context <- addin.tools::rs_get_context()
   addins_env$context <- rstudio_context
+
+  rstudioapi::sendToConsole(
+    "# To access the contents, use code: \naddins.rs:::get_saved_context()",
+    execute = FALSE
+    )
 }
 
 get_saved_context <- function() {
